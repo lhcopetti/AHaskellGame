c812a928c68c45c21859e469c01a9bd1d9ac2e74
lhcopetti <lhcopetti@gmail.com>
2018-01-09 21:46:32 -0200

    Changing Dot.hs to use the new DrawingCircle::createCircle method

--- src/GameObject/Dot.hs
+++ src/GameObject/Dot.hs
@@ -11,18 +11,17 @@ import SFML.Graphics.CircleShape
 import SFML.System.Vector2
 import SFML.Graphics.Types
 import SFML.Graphics.RenderWindow (drawCircle)
-import SFML.Graphics.SFShape (setFillColor)
 import SFML.Graphics.SFTransformable (setPosition)
 
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Maybe (MaybeT)
-import Control.Monad (mzero)
 
 import Updatable
 import Synchronizable
 import Drawable
 import Killable
 import qualified Component.Position as Comp
+import Component.Draw.CircleDrawing (createCircle)
 
 data Dot = Dot { pointer  :: CircleShape
                , position :: Vec2f
@@ -36,11 +35,10 @@ instance Updatable Dot where
     update = return
 
 instance Synchronizable Dot where
-    synchronize x = return ()
+    synchronize Dot { pointer, position } = setPosition pointer position
 
 instance Drawable Dot where 
     draw wnd Dot { pointer } = drawCircle wnd pointer Nothing
-
 
 instance Comp.Position Dot where
     getPosition = position
@@ -54,13 +52,5 @@ instance Killable Dot where
 createDot :: Vec2f -> MaybeT IO Dot
 createDot pos@(Vec2f x y) = do 
     liftIO $ putStrLn $ "Creating dot at " ++ show pos
-    myCircle <- liftIO createCircleShape
-    case myCircle of
-        Left e -> do 
-            liftIO (putStrLn $ "Error while trying to create a circle shape. " ++ show e)
-            mzero
-        Right r -> do
-            liftIO $ setFillColor r dotColor
-            liftIO $ setPosition r pos
-            liftIO $ setRadius r 5
-            return (Dot r pos True)
+    myCircle <- createCircle 5 dotColor
+    return (Dot myCircle pos True)
