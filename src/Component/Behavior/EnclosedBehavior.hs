95ec803f831d8e96024970b249c71c48b2534af9
lhcopetti <lhcopetti@gmail.com>
2018-01-12 15:23:05 -0200

    The beginning of the update method abstraction towards Behavior data types

---
+++ src/Component/Behavior/EnclosedBehavior.hs
@@ -0,0 +1,30 @@
+module Component.Behavior.EnclosedBehavior
+    ( encloseToBox
+    ) where
+
+import SFML.System.Vector2 (Vec2f)
+
+import Control.Monad.Reader (Reader, asks)
+
+import Behavior.BoxedBehavior (boundToDimension)
+import Vec2.Vec2Math (addVec2f)
+import Component.Behavior.Behavior
+import Component.Position
+import Component.Physics
+import GameEnv
+
+
+encloseToBox :: (Position a, Physics a) => a -> Reader GameEnvironment a
+encloseToBox obj = do
+        let pos = getPosition obj
+        let vel = getVelocity obj
+
+        -- Physics update
+        let newObj = setPosition obj (addVec2f pos vel)
+
+        -- Behavior update
+        dimension <- asks gameArea
+        let newObject = boundToDimension newObj dimension
+
+        -- Return the new object
+        return newObject
--- src/GameObject/Ball.hs
+++ src/GameObject/Ball.hs
@@ -6,20 +6,10 @@ module GameObject.Ball
     , synchronize
     ) where
     
-import SFML.Graphics.Color
-import SFML.Graphics.CircleShape
 import SFML.System.Vector2
-import Vec2.Vec2Math (zero, addVec2f)
 import SFML.Graphics.Types
-import SFML.Graphics.RenderWindow (drawCircle)
-import SFML.Graphics.SFShape (setFillColor)
-import SFML.Graphics.SFTransformable (setPosition)
 import Control.Monad.Trans.Maybe (MaybeT)
-import Control.Monad.IO.Class (liftIO)
-import Control.Monad.Reader.Class (asks)
-import Control.Monad (mzero)
 
-import GameEnv
 import Updatable
 import Synchronizable
 import Drawable
@@ -27,7 +17,7 @@ import Killable
 import qualified Component.Position as Pos
 import qualified Component.Physics as Phy
 import Component.Draw.Drawing
-import Behavior.BoxedBehavior (boundToDimension)
+import Component.Behavior.EnclosedBehavior
 
 data Ball = Ball { drawComp :: Drawing
                  , position :: Vec2f
@@ -36,16 +26,7 @@ data Ball = Ball { drawComp :: Drawing
                  }
 
 instance Updatable Ball where
-    update b@Ball { position, velocity } = do
-        -- Physics update
-        let newB = Pos.setPosition b (addVec2f position velocity)
-
-        -- Behavior update
-        dimension <- asks gameArea
-        let newBall = boundToDimension newB dimension
-
-        -- Return the new object
-        return newBall
+    update = encloseToBox
 
 instance Synchronizable Ball where
     synchronize ball = updateDrawing (drawComp ball) ball
