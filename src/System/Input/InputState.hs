87d9596eac3ebe7c9c6e7930d2cd0e37f9bda7fe
lhcopetti <lhcopetti@gmail.com>
2018-03-19 19:26:24 -0300

    Removing unused code from InputState

--- src/System/Input/InputState.hs
+++ src/System/Input/InputState.hs
@@ -2,12 +2,8 @@ module System.Input.InputState
     ( State (..)
     , Event (..)
     , emptyState
-    , reduceEvents
     , stepState
     ) where
-
-import SFML.Window.Event (SFEvent (..))
-import SFML.Window.Mouse
 
 data State = State  { justPressed   :: Bool
                     , isPressed     :: Bool
@@ -23,10 +19,3 @@ stepState :: State -> Event -> State
 stepState _ Pressed = State { justPressed = True, isPressed = True }
 stepState _ Released = emptyState
 stepState state Nil = emptyState { isPressed = isPressed state }
-
-reduceEvents :: [SFEvent] -> Event
-reduceEvents = foldl go Nil
-    where
-        go _ (SFEvtMouseButtonPressed MouseLeft _ _)  = Pressed
-        go _ (SFEvtMouseButtonReleased MouseLeft _ _) = Released
-        go v _ = v
