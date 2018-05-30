733504f003ff773850ae49ed449da781e516f066
lhcopetti <lhcopetti@gmail.com>
2018-05-29 21:47:15 -0300

    Remove unused imports

--- src/Component/Draw/TextureDrawing.hs
+++ src/Component/Draw/TextureDrawing.hs
@@ -5,14 +5,13 @@ module Component.Draw.TextureDrawing
 
 import SFML.Graphics.Types (Texture)
 import SFML.Graphics.Rect (IntRect)
-import SFML.Graphics.Texture (textureFromFile, textureSize)
+import SFML.Graphics.Texture (textureSize)
 import SFML.System.Vector2 (Vec2u (..))
 
 import Control.Monad.Trans.Maybe (MaybeT)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad (mzero)
 
-import Component.Draw.DrawingHelper (createShapeT)
 import GameObject.GameObjectTypes (Size (..))
 import Graphics.SFML.StaticLoader (loadTexture)
 
