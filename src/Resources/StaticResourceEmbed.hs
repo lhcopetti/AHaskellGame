5b22f341fc16a3d949ad381af4d53cec86b9274d
lhcopetti <lhcopetti@gmail.com>
2018-05-31 00:55:13 -0300

    Removing the dependency on the data-files module

--- src/Resources/StaticResourceEmbed.hs
+++ src/Resources/StaticResourceEmbed.hs
@@ -2,12 +2,8 @@ module Resources.StaticResourceEmbed
     ( resourceDirEmbedded
     ) where
 
-import Language.Haskell.TH (ExpQ, runIO)
+import Language.Haskell.TH (ExpQ)
 import Data.FileEmbed (embedDir)
 
-import Paths_AHaskellGame (getDataDir)
-
 resourceDirEmbedded :: ExpQ
-resourceDirEmbedded = do
-    dataDir <- runIO getDataDir
-    embedDir (dataDir ++ "/resources/")
+resourceDirEmbedded = embedDir "resources/"
